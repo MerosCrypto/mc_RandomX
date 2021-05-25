7d610e251fd25df54a2a26da358bd2e645761674
Luke Parker <lukeparker5132@gmail.com>
2021-05-24 22:32:04 -0500

    Remove the RandomXFlags enum as a used data type.
    I did try to set it up with a bit set, yet unfortunately, Nim has a C 
    codegen error.

--- mc_randomx.nim
+++ mc_randomx.nim
@@ -13,127 +13,126 @@ const currentFolder: string = currentSourcePath().substr(0, currentSourcePath().
 
 #Various types.
 type
-    RandomXFlags* {.importc: "randomx_flags", header: "randomx.h".} = enum
-        RANDOMX_FLAG_DEFAULT      = 0
-        RANDOMX_FLAG_LARGE_PAGES  = 1
-        RANDOMX_FLAG_HARD_AES     = 2
-        RANDOMX_FLAG_FULL_MEM     = 4
-        RANDOMX_FLAG_JIT          = 8
-        RANDOMX_FLAG_SECURE       = 16
-        RANDOMX_FLAG_ARGON2_SSSE3 = 32
-        RANDOMX_FLAG_ARGON2_AVX2  = 64
-        RANDOMX_FLAG_ARGON2       = 96
+  RandomXFlags* {.size: sizeof(cint).} = enum
+    RANDOMX_FLAG_DEFAULT      = 0
+    RANDOMX_FLAG_LARGE_PAGES  = 1
+    RANDOMX_FLAG_HARD_AES     = 2
+    RANDOMX_FLAG_FULL_MEM     = 4
+    RANDOMX_FLAG_JIT          = 8
+    RANDOMX_FLAG_SECURE       = 16
+    RANDOMX_FLAG_ARGON2_SSSE3 = 32
+    RANDOMX_FLAG_ARGON2_AVX2  = 64
 
-    RandomXCacheObject {.importc: "randomx_cache", header: "randomx.h", incompleteStruct.} = object
-    RandomXCache* = ptr RandomXCacheObject
+  RandomXCacheObject {.importc: "randomx_cache", header: "randomx.h", incompleteStruct.} = object
+  RandomXCache* = ptr RandomXCacheObject
 
-    RandomXDatasetObject {.importc: "randomx_dataset", header: "randomx.h", incompleteStruct.} = object
-    RandomXDataset* = ptr RandomXDatasetObject
+  RandomXDatasetObject {.importc: "randomx_dataset", header: "randomx.h", incompleteStruct.} = object
+  RandomXDataset* = ptr RandomXDatasetObject
 
-    RandomXVMObject {.importc: "randomx_vm", header: "randomx.h", incompleteStruct.} = object
-    RandomXVM* = ptr RandomXVMObject
+  RandomXVMObject {.importc: "randomx_vm", header: "randomx.h", incompleteStruct.} = object
+  RandomXVM* = ptr RandomXVMObject
 
 #Get the recommended flags.
-proc getFlags*(): RandomXFlags {.importc: "randomx_get_flags", header: "randomx.h".}
+proc getFlags*(): cint {.importc: "randomx_get_flags", header: "randomx.h".}
 
 #Allocate a cache.
 proc allocCache*(
-    flags: RandomXFlags
+  flags: cint
 ): RandomXCache {.importc: "randomx_alloc_cache", header: "randomx.h".}
 
 #Initiate a cache.
 proc init(
-    cache: RandomXCache,
-    key: pointer,
-    keyLen: csize
+  cache: RandomXCache,
+  key: pointer,
+  keyLen: csize
 ) {.importc: "randomx_init_cache", header: "randomx.h".}
 
 proc init*(
-    cache: RandomXCache,
-    keyArg: string
+  cache: RandomXCache,
+  keyArg: string
 ) {.inline.} =
-    var key: string = keyArg
-    if key.len == 0:
-        cache.init(nil, 0)
-    else:
-        cache.init(addr key[0], key.len)
+  var key: string = keyArg
+  if key.len == 0:
+    cache.init(nil, 0)
+  else:
+    cache.init(addr key[0], key.len)
 
 #Deallocate a cache.
 proc dealloc*(
-    cache: RandomXCache
+  cache: RandomXCache
 ) {.importc: "randomx_release_cache", header: "randomx.h".}
 
 #Allocate a dataset.
 proc allocDataset*(
-    flags: RandomXFlags
+  flags: cint
 ): RandomXDataset {.importc: "randomx_alloc_dataset", header: "randomx.h".}
 
 #Initiate a dataset.
 proc init*(
-    dataset: RandomXDataset,
-    cache: RandomXCache,
-    startItem: culong,
-    itemCount: culong
+  dataset: RandomXDataset,
+  cache: RandomXCache,
+  startItem: culong,
+  itemCount: culong
 ) {.importc: "randomx_init_dataset", header: "randomx.h".}
 
 #Deallocate a dataset.
 proc dealloc*(
-    dataset: RandomXDataset
+  dataset: RandomXDataset
 ) {.importc: "randomx_release_dataset", header: "randomx.h".}
 
 #Create a VM.
 proc createVM*(
-    flags: RandomXFlags,
-    cache: RandomXCache,
-    dataset: RandomXDataset
+  flags: cint,
+  cache: RandomXCache,
+  dataset: RandomXDataset
 ): RandomXVM {.importc: "randomx_create_vm", header: "randomx.h".}
 
 #Set the VM's cache.
 proc setCache*(
-    vm: RandomXVM,
-    cache: RandomXCache
+  vm: RandomXVM,
+  cache: RandomXCache
 ) {.importc: "randomx_vm_set_cache", header: "randomx.h".}
 
 #Set the VM's dataset.
 proc setDataset*(
-    vm: RandomXVM,
-    dataset: RandomXDataset
+  vm: RandomXVM,
+  dataset: RandomXDataset
 ) {.importc: "randomx_vm_set_dataset", header: "randomx.h".}
 
 #Destroy a VM.
 proc destroy*(
-    vm: RandomXVM
+  vm: RandomXVM
 ) {.importc: "randomx_destroy_vm", header: "randomx.h".}
 
 #Hash data.
 proc hash(
-    vm: RandomXVM,
-    input: pointer,
-    inputLen: csize,
-    output: pointer
+  vm: RandomXVM,
+  input: pointer,
+  inputLen: csize,
+  output: pointer
 ) {.importc: "randomx_calculate_hash", header: "randomx.h".}
 
 proc hash*(
-    vm: RandomXVM,
-    inputArg: string
+  vm: RandomXVM,
+  inputArg: string
 ): string =
-    var input: string = inputArg
-    result = newString(32)
-    if input.len == 0:
-        vm.hash(nil, 0, addr result[0])
-    else:
-        vm.hash(addr input[0], input.len, addr result[0])
+  var input: string = inputArg
+  result = newString(32)
+  if input.len == 0:
+    vm.hash(nil, 0, addr result[0])
+  else:
+    vm.hash(addr input[0], input.len, addr result[0])
 
 #Hash data yet optimized for when there's a series of hashes.
 proc hashFirst*(
-    vm: RandomXVM,
-    input: pointer,
-    inputLen: csize
+  vm: RandomXVM,
+  input: pointer,
+  inputLen: csize
 ) {.importc: "randomx_calculate_hash_first", header: "randomx.h".}
 
 proc hashNext*(
-    vm: RandomXVM,
-    input: pointer,
-    inputLen: csize,
-    output: pointer
+  vm: RandomXVM,
+  input: pointer,
+  inputLen: csize,
+  output: pointer
 ) {.importc: "randomx_calculate_hash_next", header: "randomx.h".}
--- mc_randomx.nimble
+++ mc_randomx.nimble
@@ -1,6 +1,6 @@
 import os
 
-version     = "0.9.6"
+version     = "0.9.7"
 author      = "Luke Parker"
 description = "A Nim wrapper for RandomX with the configuration used by the Meros Cryptocurrency."
 license     = "MIT"
@@ -8,31 +8,31 @@ license     = "MIT"
 requires "nim >= 1.0.4"
 
 installDirs = @[
-    "RandomX"
+  "RandomX"
 ]
 
 installFiles = @[
-    "mc_randomx.nim"
+  "mc_randomx.nim"
 ]
 
 before install:
-    let cmakeExe: string = system.findExe("cmake")
-    if cmakeExe == "":
-        echo "Failed to find executable `cmake`."
-        quit(1)
+  let cmakeExe: string = system.findExe("cmake")
+  if cmakeExe == "":
+    echo "Failed to find executable `cmake`."
+    quit(1)
 
-    let makeExe: string = system.findExe("make")
-    if makeExe == "":
-        echo "Failed to find executable `make`."
-        quit(1)
+  let makeExe: string = system.findExe("make")
+  if makeExe == "":
+    echo "Failed to find executable `make`."
+    quit(1)
 
-    withDir thisDir() / "RandomX" / "src":
-        rmFile "configuration.h"
-        cpFile("../../MerosConfiguration/configuration.h", "./configuration.h")
+  withDir thisDir() / "RandomX" / "src":
+    rmFile "configuration.h"
+    cpFile("../../MerosConfiguration/configuration.h", "./configuration.h")
 
-    withDir thisDir() / "RandomX":
-        mkDir "build"
+  withDir thisDir() / "RandomX":
+    mkDir "build"
 
-    withDir thisDir() / "RandomX" / "build":
-        exec cmakeExe & " .."
-        exec makeExe
+  withDir thisDir() / "RandomX" / "build":
+    exec cmakeExe & " .."
+    exec makeExe
